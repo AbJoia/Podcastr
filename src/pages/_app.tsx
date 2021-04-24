514a79ae2543b4df4bf7c95bb0d08eb5d3c672d9
Abner Joia <t2m.ajoia@terceiros.mag.com.br>
2021-04-24 15:52:54 -0300

    app podcastr

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import '../styles/global.scss';
+import styles from '../styles/app.module.scss';
+
+import { Header } from '../components/Header';
+import { Player } from '../components/Player';
+import { PlayerContextProvider } from '../context/PlayerContext';
+
+function MyApp({ Component, pageProps }) {
+  return(
+    <PlayerContextProvider>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContextProvider>    
+  );
+}
+
+export default MyApp
